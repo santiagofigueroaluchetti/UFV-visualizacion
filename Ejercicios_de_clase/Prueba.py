a315eb6142e8a0cdebb8af4f3eaf2e03e4f8e2bc
santiagofigueroaluchetti <144936237+santiagofigueroaluchetti@users.noreply.github.com>
2024-10-11 17:07:29 +0100

    Creado con Colab

--- Ejercicios_de_clase/Prueba.py
+++ Ejercicios_de_clase/Prueba.py
@@ -1,1 +1,39 @@
-
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyNyz8dOGqKeqpnC8X+fLyNT",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/santiagofigueroaluchetti/UFV-visualizacion/blob/main/Ejercicios_de_clase/Prueba.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "22ajYfDxPci5"
+      },
+      "outputs": [],
+      "source": []
+    }
+  ]
+}
